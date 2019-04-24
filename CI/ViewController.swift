03b1df8dca113a5d26aa2b895c5ded0ec2588e3a
naccache <naccache@mobiblanc.com>
2019-04-24 11:38:01 +0100

    implement alert on button click

--- CI/ViewController.swift
+++ CI/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - ACTIONS
+    @IBAction func clickMed(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "You clicked Me", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - VIEW CONTROLLER METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
