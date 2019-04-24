b1e6488c921ef4518e148a945e9b13eac772fdc6
naccache <naccache@mobiblanc.com>
2019-04-24 11:15:17 +0100

    Initial Commit

---
+++ CI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI
+//
+//  Created by Mobiblanc on 24/04/2019.
+//  Copyright © 2019 Mobiblanc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
